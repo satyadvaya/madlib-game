e556417490b0bfc1bbcd14df30f6b2c21e17af44
satyadvaya <satyadvaya@gmail.com>
2021-02-12 19:01:04 -0800

    added variables for all inputs and spans

--- app.js
+++ app.js
@@ -3,3 +3,60 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const timeOfDayInput = document.getElementById('timeOfDay-input');
+const timeOfDaySpan = document.getElementById('timeOfDay-span');
+
+const timeOfDayInput = document.getElementById('holiday-input');
+const timeOfDaySpan = document.getElementById('holiday-span');
+
+const timeOfDayInput = document.getElementById('typeOfAbode-input');
+const timeOfDaySpan = document.getElementById('typeOfAbode-span');
+
+const timeOfDayInput = document.getElementById('animal1-input');
+const timeOfDaySpan = document.getElementById('animal1-span');
+
+const timeOfDayInput = document.getElementById('verb1-input');
+const timeOfDaySpan = document.getElementById('verb1-span');
+
+const timeOfDayInput = document.getElementById('animal2');
+const timeOfDaySpan = document.getElementById('animal2-span');
+
+const timeOfDayInput = document.getElementById('clothing1-input');
+const timeOfDaySpan = document.getElementById('clothing1-span');
+
+const timeOfDayInput = document.getElementById('partOfHouse-input');
+const timeOfDaySpan = document.getElementById('partOfHouse-span');
+
+const timeOfDayInput = document.getElementById('mythicalCreature-input');
+const timeOfDaySpan = document.getElementById('mythicalCreature-span');
+
+const timeOfDayInput = document.getElementById('activeVerb-input');
+const timeOfDaySpan = document.getElementById('activeVerb-span');
+
+const timeOfDayInput = document.getElementById('feistyCreatures-input');
+const timeOfDaySpan = document.getElementById('feistyCreatures-span');
+
+const timeOfDayInput = document.getElementById('loungyFurniture-input');
+const timeOfDaySpan = document.getElementById('loungyFurniture-span');
+
+const timeOfDayInput = document.getElementById('fruit-input');
+const timeOfDaySpan = document.getElementById('fruit-span');
+
+const timeOfDayInput = document.getElementById('bodyPart1-input');
+const timeOfDaySpan = document.getElementById('bodyPart1-span');
+
+const timeOfDayInput = document.getElementById('familyMember-input');
+const timeOfDaySpan = document.getElementById('familyMember-span');
+
+const timeOfDayInput = document.getElementById('clothing2-input');
+const timeOfDaySpan = document.getElementById('clothing2-span');
+
+const timeOfDayInput = document.getElementById('clothing3-input');
+const timeOfDaySpan = document.getElementById('clothing3-span');
+
+const timeOfDayInput = document.getElementById('bodyPart2-input');
+const timeOfDaySpan = document.getElementById('bodyPart2-span');
+
+const timeOfDayInput = document.getElementById('noun-input');
+const timeOfDaySpan = document.getElementById('noun-span');
