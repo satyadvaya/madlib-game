3b534d09b6108609579e0d94102adc71d4171b93
satyadvaya <satyadvaya@gmail.com>
2021-02-12 20:22:06 -0800

    corrected variable assignments and added ones for madlib and input containers as well as the button

--- app.js
+++ app.js
@@ -7,56 +7,76 @@
 const timeOfDayInput = document.getElementById('timeOfDay-input');
 const timeOfDaySpan = document.getElementById('timeOfDay-span');
 
-const timeOfDayInput = document.getElementById('holiday-input');
-const timeOfDaySpan = document.getElementById('holiday-span');
+const holidayInput = document.getElementById('holiday-input');
+const holidaySpan = document.getElementById('holiday-span');
 
-const timeOfDayInput = document.getElementById('typeOfAbode-input');
-const timeOfDaySpan = document.getElementById('typeOfAbode-span');
+const typeOfAbodeInput = document.getElementById('typeOfAbode-input');
+const typeOfAbodeSpan = document.getElementById('typeOfAbode-span');
 
-const timeOfDayInput = document.getElementById('animal1-input');
-const timeOfDaySpan = document.getElementById('animal1-span');
+const animal1Input = document.getElementById('animal1-input');
+const animal1Span = document.getElementById('animal1-span');
 
-const timeOfDayInput = document.getElementById('verb1-input');
-const timeOfDaySpan = document.getElementById('verb1-span');
+const verb1Input = document.getElementById('verb1-input');
+const verb1Span = document.getElementById('verb1-span');
 
-const timeOfDayInput = document.getElementById('animal2');
-const timeOfDaySpan = document.getElementById('animal2-span');
+const animal2Input = document.getElementById('animal2');
+const animal2Span = document.getElementById('animal2-span');
 
-const timeOfDayInput = document.getElementById('clothing1-input');
-const timeOfDaySpan = document.getElementById('clothing1-span');
+const clothing1Input = document.getElementById('clothing1-input');
+const clothing1Span = document.getElementById('clothing1-span');
 
-const timeOfDayInput = document.getElementById('partOfHouse-input');
-const timeOfDaySpan = document.getElementById('partOfHouse-span');
+const partOfHouseInput = document.getElementById('partOfHouse-input');
+const partOfHouseSpan = document.getElementById('partOfHouse-span');
 
-const timeOfDayInput = document.getElementById('mythicalCreature-input');
-const timeOfDaySpan = document.getElementById('mythicalCreature-span');
+const mythicalCreatureInput = document.getElementById('mythicalCreature-input');
+const mythicalCreatureSpan = document.getElementById('mythicalCreaturepan');
 
-const timeOfDayInput = document.getElementById('activeVerb-input');
-const timeOfDaySpan = document.getElementById('activeVerb-span');
+const activeVerbInput = document.getElementById('activeVerb-input');
+const activeVerbSpan = document.getElementById('activeVerb-span');
 
-const timeOfDayInput = document.getElementById('feistyCreatures-input');
-const timeOfDaySpan = document.getElementById('feistyCreatures-span');
+const fiestyCreaturesInput = document.getElementById('feistyCreatures-input');
+const fiestyCreaturesSpan = document.getElementById('feistyCreatures-span');
 
-const timeOfDayInput = document.getElementById('loungyFurniture-input');
-const timeOfDaySpan = document.getElementById('loungyFurniture-span');
+const loungyFurnitureInput = document.getElementById('loungyFurniture-input');
+const loungyFurnitureSpan = document.getElementById('loungyFurniture-span');
 
-const timeOfDayInput = document.getElementById('fruit-input');
-const timeOfDaySpan = document.getElementById('fruit-span');
+const fruitInput = document.getElementById('fruit-input');
+const fruitSpan = document.getElementById('fruit-span');
 
-const timeOfDayInput = document.getElementById('bodyPart1-input');
-const timeOfDaySpan = document.getElementById('bodyPart1-span');
+const bodyPart1Input = document.getElementById('bodyPart1-input');
+const bodyPart1Span = document.getElementById('bodyPart1-span');
 
-const timeOfDayInput = document.getElementById('familyMember-input');
-const timeOfDaySpan = document.getElementById('familyMember-span');
+const familyMemberInput = document.getElementById('familyMember-input');
+const familyMemberSpan = document.getElementById('familyMember-span');
 
-const timeOfDayInput = document.getElementById('clothing2-input');
-const timeOfDaySpan = document.getElementById('clothing2-span');
+const clothing2Input = document.getElementById('clothing2-input');
+const clothing2Span = document.getElementById('clothing2-span');
 
-const timeOfDayInput = document.getElementById('clothing3-input');
-const timeOfDaySpan = document.getElementById('clothing3-span');
+const clothing3Input = document.getElementById('clothing3-input');
+const clothing3Span = document.getElementById('clothing3-span');
 
-const timeOfDayInput = document.getElementById('bodyPart2-input');
-const timeOfDaySpan = document.getElementById('bodyPart2-span');
+const bodyPart2Input = document.getElementById('bodyPart2-input');
+const bodyPart2Span = document.getElementById('bodyPart2-span');
 
-const timeOfDayInput = document.getElementById('noun-input');
-const timeOfDaySpan = document.getElementById('noun-span');
+const nounInput = document.getElementById('noun-input');
+const nounSpan = document.getElementById('noun-span');
+
+const madlibContainer = document.getElementById('madlib-container');
+
+const inputContainer = document.getElementById('input-container');
+
+const buttonInput = document.getElementById('button-input');
+
+buttonInput.addEventListener('click', () => {
+    timeOfDayInput.value = timeOfDaySpan.textContent;
+
+    // const divideTotal = Number(firstDivideInput.value) / Number(secondDivideInput.value);
+    // quotientDisplay.textContent = divideTotal;
+});
+
+export function sync(input, span){
+    span.textContent = input.value;
+}
+
+divideButton.addEventListener('click', () => {
+    quotientDisplay.textContent = divide(Number(firstDivideInput.value), Number(secondDivideInput.value));
